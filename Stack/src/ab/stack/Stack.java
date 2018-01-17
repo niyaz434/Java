987e68e1eb4b256f5203cb09e668d31118e26e9e
Niyaz <niyaz434@gmail.com>
2018-01-17 18:38:55 +0530

    Stack section was completed

---
+++ Stack/src/ab/stack/Stack.java
@@ -0,0 +1,43 @@
+package ab.stack;
+
+public class Stack {
+
+	private int maxSize;
+	private char[] stackArray;
+	private int top;
+	
+	public Stack(int maxSize){
+		this.maxSize =maxSize;
+		this.stackArray =new char[maxSize];
+		this.top =-1;
+		
+	}
+	public void push(char j){
+		if(isFull()){
+			System.out.println("Stack is Full");
+		}else{
+		top++;
+		stackArray[top] =j;
+		}
+	}
+	public char pop(){
+		if(isEmpty()){
+			System.out.println("Stack is empty");
+			return 'O';
+		}else{
+		int old_top =top;
+		top--;
+		return stackArray[old_top];
+		}
+	}
+	
+	public boolean isEmpty(){
+		return (top == -1);
+	}
+	
+	public boolean isFull(){
+		return(maxSize-1 == top);
+	}
+	
+	
+}
